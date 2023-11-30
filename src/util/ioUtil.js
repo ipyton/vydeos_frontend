02c9c896c627d28ad9716c93c2cdc8cfe32551ce
ipyton <czhdawng@163.com>
2023-11-30 22:44:07 +0800

    Add utils for the project.

--- src/util/ioUtil.js
+++ src/util/ioUtil.js
@@ -1,20 +1,105 @@
 import axios  from "axios"
 import Qs from 'qs'
 
-export default function verifyTokens(token) {
-    async function post(){
-        let response = await axios({
-            url:"http://localhost:8080/account/verifyToken", 
-            method:'post',
-            data:{token:token},
-            transformRequest:[function (data) {
-              return Qs.stringify(data)
-          }],
-          headers:{token:token}
-        })
-        let responseData = response.data
-        console.log(response)
-        return responseData.code === 1
+export default class IOUtil {
+    static verifyTokens(token) {
+        async function post(){
+            let response = await axios({
+                url:"http://localhost:8080/account/verifyToken", 
+                method:'post',
+                data:{token:localStorage.getItem("token")},
+                transformRequest:[function (data) {
+                  return Qs.stringify(data)
+              }],
+              headers:{token: localStorage.getItem("token"),
+                        'userEmail': '1838169994@qq.com'}
+            })
+            let responseData = response.data
+            console.log(response)
+            return responseData.code === 1
+        }
+        return post()
     }
-    return post()
+    
+    static uploadArticle() {
+        async function post(){
+            let response = await axios({
+                url:"http://localhost:8080/article/upload_article", 
+                method:'post',
+                data:{token:localStorage.getItem("token")},
+                transformRequest:[function (data) {
+                  return Qs.stringify(data)
+              }],
+              headers:{token:localStorage.getItem("token"),
+                        'userEmail': '1838169994@qq.com'}
+            })
+            let responseData = response.data
+            console.log(response)
+            return responseData.code === 1
+        }
+        return post()
+    }
+
+    static getArticle(articleID) {
+        async function post(){
+            let response = await axios({
+                url:"http://localhost:8080/article/get_article", 
+                method:'post',
+                data:{token:localStorage.getItem("token")},
+                transformRequest:[function (data) {
+                  return Qs.stringify(data)
+              }],
+              headers:{token:localStorage.getItem("token"),
+                        'userEmail': '1838169994@qq.com'}
+            })
+            let responseData = response.data
+            console.log(response)
+            return responseData.code === 1
+        }
+        return post()
+    }
+
+    static getArticlesByUserID(userID, from, to) {
+        async function post(){
+            let response = await axios({
+                url:"http://localhost:8080/article/get_articles_range", 
+                method:'post',
+                data:{token:localStorage.getItem("token"),
+                        'userEmail': '1838169994@qq.com',
+                        'from':from,
+                        'to':to},
+                transformRequest:[function (data) {
+                  return Qs.stringify(data)
+              }],
+              headers:{token:localStorage.getItem("token"),
+                        'userEmail': '1838169994@qq.com'}
+            })
+            let responseData = response.data
+            console.log(response)
+            return responseData.code === 1
+        }
+        return post()
+    }
+
+    static getArticleByID(articleID) {
+        async function post(){
+            let response = await axios({
+                url:"http://localhost:8080/article/get_recommend_articles", 
+                method:'post',
+                data:{token: localStorage.getItem("token")},
+                transformRequest:[function (data) {
+                  return Qs.stringify(data)
+              }],
+              headers:{token: localStorage.getItem("token"),
+                        'userEmail': '1838169994@qq.com'}
+            })
+            let responseData = response.data
+            console.log(response)
+            return responseData.code === 1
+        }
+        return post()
+    }
 }
+
+
+
