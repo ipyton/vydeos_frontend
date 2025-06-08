bebacc0416cf72f77a0a39c6d67709e4a9ee1bf5
ipyton <czhdawang@163.com>
2025-06-08 13:03:21 +1000

    refactoring the message util code

--- src/util/io_utils/MessageUtil.js
+++ src/util/io_utils/MessageUtil.js
@@ -78,110 +78,23 @@ export default class MessageUtil {
     }
     
     static getMessageRecords(type,friendId, limit, lastSessionMessageId) {
-        let checkKey = "chatLastUpdate";
-        localforage.getItem(checkKey).then(async timestamp => {
-            if (!timestamp) {
-                timestamp = 0;
-            }
             
-            const requestData = Qs.stringify({
-                "userId": friendId, 
-                "timestamp": timestamp,
-                "limit":limit,
-                "lastSessionMessageId":lastSessionMessageId
-            });
-
-            apiClient.post("/chat/getMessageRecords", requestData, {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            }).then(async (response) => {
-                // All the commented code remains the same
-                //MessageUtil.updateMessage(response)
-
-                // if (!response) {
-                //     return
-                // }
-                // if (!response.data) {
-                //     console.log("error")
-                //     return
-                // }
-
-                // if (response.data.code !== 1) {
-                //     console.log("logic error")
-                //     return
-                // }
-                // const messages = JSON.parse(response.data.message)
-                // let records_map = {}
-
-                // //dispatch messages to their sender in a map. 
-                // let maxTimestamp = timestamp
-                // for (let i = 0; i < messages.length; i++) {
-                //     let userId = messages[i].userId;
-                //     if (records_map[userId]) {
-                //         records_map[userId].push(messages[i])
-                //     } else {
-                //         records_map[userId] = [messages[i]]
-                //     }
-                //     maxTimestamp = Math.max(messages[i].sendTime, maxTimestamp)
-                // }
-                // await localforage.setItem(checkKey, maxTimestamp)
-
-                // //write to the database
-                // for (let key in records_map) {
-                //     let value = records_map[key]
-                //     let result = await localforage.getItem(key + "_records")
-                //     if (!result) {
-                //         result = { count: value.size, chats: value }
-                //     }
-                //     else {
-                //         result.count++
-                //         result.chats = [result.chats, ...value]
-                //     }
-                //     await localforage.setItem("send_from_" + friendId, result.chats)
-                // }
-
-                // get chat record contacts lists
-                // await localforage.getItem("contactCursor").then((userId) => {
-                //     if (!userId) {
-                //         localforage.getItem("contactRecordList").then((result) => {
-
-                //         })
-                //         return
-                //     }
-                //     localforage.getItem("contactRecordList").catch(() => {
-                //     }).then(async (result) => {
-                //         if (!result) {
-                //             return
-                //         }
-                //         let contains = -1
-                //         for (let i = 0; i < result.length; i++) {
-                //             if (result[i].userId === userId) {
-                //                 contains = i;
-                //             }
-                //         }
-                //         // contactDetails
-                //         // contactRecordList [{userId:userId, avatar:avatar, userName}, xxx, xxx]
-                //         let detail = await localforage.getItem(userId + "_detail")
-                //         if (contains === -1) {//query from localforage and set information
-                //             if (!detail) {
-                //                 console.log("he is not your friend!!!!")
-                //                 return
-                //             }
-                //             result = [...result, { userName: detail.userName, avatar: detail.avatar, userId: detail.userId }]
-                //             localforage.setItem("contactRecordList", result)
-                //         }
-                //         else {
-                //             result = [...result, { userName: detail.userName, avatar: detail.avatar, userId: detail.userId }]
-                //             localforage.setItem("contactRecordList", result)
-                //         }
-                //     }
-                //     )
-                // })
-            }).then(() => {
-                //setSelect(friendId)
-            });
+        const requestData = Qs.stringify({
+            "userId": friendId, 
+            "limit":limit,
+            "lastSessionMessageId":lastSessionMessageId
         });
+
+        return apiClient.post("/chat/getMessageRecords", requestData, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(async (response) => {
+            console.log(response)
+        }).then(() => {
+            //setSelect(friendId)
+        });
+
     }
 
     static sendMessage(userId, sendTo, content, messageType) {
